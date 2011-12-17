52335ece00edc895b10b346a4cc268189045e14b
Seppo Sinisalo <ssinisal@cs.helsinki.fi>
2011-12-18 00:48:34 +0200

    esa

--- AN/AN/Screens/Nerd.cs
+++ AN/AN/Screens/Nerd.cs
@@ -29,7 +29,7 @@ namespace AN
         Vector2 liike;
 
         float vauhti = 0;
-        double aika = -5;
+        double aika = 0;
 		int testi;
 
        public  Boolean osui = false; //checks if game is in state where it should move to next level.
@@ -110,7 +110,6 @@ namespace AN
                 ammuttu = true;
                 laskeliike();
             }
-
             //vielä yksi metodi lisää jossa painettuna on true, jotta saadaan nörtti liikkumaan hiiren mukana kun nappi painettuna
             if (aCurrentMouseState.LeftButton == ButtonState.Pressed && painettuna == true && ammuttu == false)
             {
@@ -129,58 +128,43 @@ namespace AN
 
             if ( ammuttu == true && maassa ==false)
             {
-                laskeAmmuksenLentorata();
-                    
+                laskeAmmuksenLentorata();             
+            }
+            if (ammuttu == true && maassa == true)
+            {
+
             }
          
-        }
-
-
+        }      
         private void laskeliike()
         {
-            liike.X = Math.Abs(mouseStart.X - mouseEnd.X)/5;
-            liike.Y = Math.Abs(mouseStart.Y - mouseEnd.Y)/5;
-            
-         
+            liike.X = Math.Abs(mouseStart.X - mouseEnd.X)/10;
+            liike.Y = Math.Abs(mouseStart.Y - mouseEnd.Y)/10;     
         }
-
         private void laskeAmmuksenLentorata()
-        {
-
-            
+        {         
             double x = 0;
-            double y = 0;
-            
-            double painovoimakiihtyvyys = 0.98;
-            liike.X = 2; //kertoo kuinka jyrkästi x-akselin suuntaan ammus lähtee.. mitä isompi niin sitä jyrkempi
-            liike.Y = 30; //kertoo kuinka jyrkkään y akselin suuntaan ammus lähtee.. mitä pienempi arvo niin sitä jyrkempi
+            double y = 0;           
+            double painovoimakiihtyvyys = 2;
+             
+            y = -(liike.Y * aika) + 645 + (aika*aika*painovoimakiihtyvyys);
 
-            int apuX = 0; //HelpIntegers to define angle of shot
-            int apuY = 0;
+           
+            x = liike.X * aika+125;
 
-            apuX  = (int)(mouseStart.X - mouseEnd.X) / 80;
-            liike.X = apuX;
-
-            apuY = (int)(mouseEnd.Y - mouseStart.Y)/2;
-            liike.Y = apuY;
-
-            // Ammuksen lentorata on paraabeli, jonka y-arvo lasketaan kaavalla:
-            //y = t * ut
-            x = ((liike.X * aika) * aika)+645;
-
-            // ja x-arvo lasketaan kaavalla:
-            y =  (painovoimakiihtyvyys * (aika * aika)) + (liike.Y * aika )-400 ;
-
-           nerdPosition.Y = (int)x + mouseEnd.X;
-           nerdPosition.X = (int)(y + mouseEnd.Y);
+            nerdPosition.Y = (int)y;
+            nerdPosition.X = (int)x;
 
 
            //kertoo ammuksen nopeuden ajan suhteen. tämän voisi fixata toimimaan gametimen perusteella, mutta on nyt vakio testausta varten
-           aika= (aika+0.08);
+           aika= (aika+0.16);
 
             //tähän pitäisi hahmotella jokin systeemi huomaamaan jos ammus on jo maassa.
-           if (nerdPosition.Y == 800) maassa = true;
-       
+           if (nerdPosition.Y > 700)
+           {
+               maassa = true;
+               nerdPosition.Y = 700;
+           }
         }
 
         public void toNextLevel(){
